fd31d6228c521c2552412d79aa98bc10054f6f15
gustavo <penarandagustavo5@gmail.com>
2022-09-10 15:08:59 -0500

    my all method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,33 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    for i in (0..(self.length - 1))
+      yield self[i], i
+    end
+    self
+  end
+
+  def my_select
+    result = []
+    for i in self
+      result << i if yield i
+    end
+    result
+  end
+
+  def my_all?(pat = TrueClass)
+    for i in self
+      if block_given?
+        result = yield i
+      elsif pat == TrueClass
+        result = ((i&&true) in ^pat)
+      else
+        result = (i in ^pat)
+      end
+      result ? next : break
+    end
+    result
+  end
 end
 
 # You will first have to define my_each
@@ -12,5 +40,6 @@ class Array
     for i in self
       yield i
     end
+    self
   end
 end
